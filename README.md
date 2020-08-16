d230c56e560f0e055a8f355b15691d5d8f7b9fcf
Fernando Devegili <fedevegili@gmail.com>
2020-08-16 18:39:03 -0300

    First version

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-Used by visual regression testing to take images from testing scenarios.
+### serverless-backstopjs
+[BackstopJS](https://github.com/garris/BackstopJS) on rockets :rocket::rocket::rocket:! This package creates a serverless function that receives BackstopJS config JSON and run it on the cloud!
+
+I wrote an article on how to take 1000 images in 10 seconds here: ARTICLE_LINK
 
 ### How to deploy?
-```serverless deploy --stage dev```
+```npm install```
+```npx serverless deploy```
 
-### How to view logs?
-```serverless logs -f serverless-backstopjs-fn```
+### How does it work?
+Using [chrome-aws-lambda](https://github.com/alixaxel/chrome-aws-lambda) to provide a chrome to [BackstopJS](https://github.com/garris/BackstopJS) to control.
+
+### How to use?
+Check this article: ARTICLE_LINK
--- handler.js
+++ handler.js
@@ -2,61 +2,78 @@ const fs = require('fs');
 const runner = require('backstopjs/core/runner.js');
 
 module.exports.index = async (event) => {
-  console.log(`Received request`);
+	console.log('Received request');
 
-  let data;
+	let data;
 
-  try {
-    data = JSON.parse(event.body);
-  } catch(e) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        error: 'unreadable body',
-      }),
-    };
-  }
+	try {
+		data = JSON.parse(event.body);
+	} catch (e) {
+		return {
+			statusCode: 500,
+			body: JSON.stringify({
+				error: 'unreadable body',
+			}),
+		};
+	}
 
-  const rand = Math.floor(Math.random() * 10000000000) + 1;
-  const baseConfig = `/tmp/backstop/${rand}/`;
-  const basePath = '/tmp/tested-images';
+	/**
+   * We have to create a random directory for each run
+   * because of how backstopjs works.
+   * Basically, it uses nodejs "require" to load the dependant JS (e.g.: onReadyScript)
+   * This means that the script content may get cached between calls, which we never want.
+   */
+	const rand = Math.floor(Math.random() * 10000000000);
+	const baseConfig = `/tmp/backstop/${rand}/`;
+	const basePath = '/tmp/tested-images';
 
-  if (!fs.existsSync('/tmp/backstop')) {
-    fs.mkdirSync('/tmp/backstop');
-  }
+	if (!fs.existsSync('/tmp/backstop')) {
+		fs.mkdirSync('/tmp/backstop');
+	}
 
-  if (!fs.existsSync(baseConfig)) {
-    fs.mkdirSync(baseConfig);
-  }
+	if (!fs.existsSync(baseConfig)) {
+		fs.mkdirSync(baseConfig);
+	}
 
-  Object.keys(data).forEach((fileName) => {
-    let writeData = data[fileName];
+	/**
+   * Update backstop.json config to change some paths.
+   * Serverless functions can only access /tmp, so this is a must
+   */
+	Object.keys(data).forEach((fileName) => {
+		let writeData = data[fileName];
 
-    if (fileName === 'backstop.json') {
-      writeData = JSON.parse(writeData);
-      writeData.paths.engine_scripts = baseConfig;
-      writeData = JSON.stringify(writeData);
-    }
+		if (fileName === 'backstop.json') {
+			writeData = JSON.parse(writeData);
+			writeData.paths.engine_scripts = baseConfig;
+			writeData = JSON.stringify(writeData);
+		}
 
-    fs.writeFileSync(baseConfig + fileName, writeData);
-  });
+		fs.writeFileSync(baseConfig + fileName, writeData);
+	});
 
-  await runner('reference', {
-    config: baseConfig + 'backstop.json'
-  });
+	try {
+		await runner('reference', {
+			config: `${baseConfig}backstop.json`,
+		});
+	} catch (error) {
+		return {
+			statusCode: 500,
+			body: JSON.stringify(error),
+		};
+	}
 
-  const files = fs.readdirSync(basePath);
-  const fileObj = {};
+	const files = fs.readdirSync(basePath);
+	const fileObj = {};
 
-  files.forEach((fileName) => {
-    const bitmap = fs.readFileSync(`${basePath}/${fileName}`);
-    fileObj[fileName] = new Buffer(bitmap).toString('base64');
-  });
+	files.forEach((fileName) => {
+		const bitmap = fs.readFileSync(`${basePath}/${fileName}`);
+		fileObj[fileName] = new Buffer(bitmap).toString('base64');
+	});
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      result: fileObj
-    }),
-  };
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+			result: fileObj,
+		}),
+	};
 };
